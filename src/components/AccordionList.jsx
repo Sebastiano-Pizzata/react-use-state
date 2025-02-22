0a8e0111bb9ee55d220afc4fd5db222503d32801
Sebastiano-Pizzata <rsebastiano23@gmail.com>
2025-02-22 17:50:34 +0100

    ADD: accordions and temporary style

--- src/components/AccordionList.jsx
+++ src/components/AccordionList.jsx
@@ -1,5 +1,31 @@
-function accordionList() {
+import { useState } from "react"
+
+const AccordionList = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const { faq } = props;
+    const { title, description } = faq;
+
+    const handleAccordion = () => setIsOpen(!isOpen);
+
+    return (
+        <>
+            <div className=" m-3">
+                <button className="btn bg-primary text-white" onClick={handleAccordion}>{title}</button>
+            </div>
+            <div className=" mt-5">
+                <div className="card">
+                    {isOpen && <h4 className="p-2">{title} </h4>}
+                    {isOpen && <p className="p-2">{description}</p>}
+                </div>
+            </div>
+
+
+        </>
+
+
+    )
+
 
 }
 
-export default accordionList
+export default AccordionList
